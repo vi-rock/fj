7b5d904338e3e37842e971deb55c7906462bf05d
Иванов Владислав <ivanov.vlad135@gmail.com>
2026-07-22 00:30:14 +0500

    feat: logging

--- packages/observability/src/observability/logging/config.py
+++ packages/observability/src/observability/logging/config.py
@@ -1,24 +1,59 @@
-import logging
+from logging import config as logging_config
 
 import structlog
+from structlog.types import Processor
 
 
 def configure_logging(
     level: str,
     json_logs: bool,
 ) -> None:
-    logging.basicConfig(
-        level=level,
-    )
+    shared_processors: list[Processor] = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+    ]
 
-    renderer = structlog.processors.JSONRenderer() if json_logs else structlog.dev.ConsoleRenderer()
+    renderer: Processor
+
+    if json_logs:
+        renderer = structlog.processors.JSONRenderer()
+    else:
+        renderer = structlog.dev.ConsoleRenderer(colors=True)
+
+    logging_config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "foreign_pre_chain": shared_processors,
+                    "processors": [
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        renderer,
+                    ],
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                },
+            },
+            "root": {
+                "handlers": ["default"],
+                "level": level,
+            },
+        }
+    )
 
     structlog.configure(
         processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.processors.add_log_level,
-            structlog.processors.TimeStamper(fmt="iso"),
-            renderer,
+            *shared_processors,
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
